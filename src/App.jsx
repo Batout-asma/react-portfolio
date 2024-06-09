8403f0de70ed4aa4110f4106492027cfd8cebefe
Asma <benramoulasma@gmail.com>
2024-06-09 17:44:48 +0200

    fix the layout

--- src/App.jsx
+++ src/App.jsx
@@ -33,12 +33,30 @@ export default function App() {
           </div>
         </div>
       </div>
-      <div className="about">
-        <div className="container">
-          <div className="row">
-            <img src="/images/user.png" className="" />
-            <div className="about-col-1"></div>
-            <div className="about-col-2"></div>
+      {/* -- -------------- about -------------- -- */}
+      <div className="about py-20 text-[#ababab]">
+        <div className="container pl-[150px]">
+          <div className="row flex flex-wrap justify-between">
+            <div className="about-col-1 basis-[35%]">
+              <img src="/images/user.png" className="w-full rounded-2xl" />
+            </div>
+            <div className="about-col-2 basis-[60%]">
+              <h1 className="sub-title  mb-5 text-6xl font-semibold text-[#fff]">
+                About Me
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                lobortis consequat mauris non fringilla. Aenean iaculis augue id
+                tellus varius, et mattis nulla facilisis. Aliquam erat volutpat.
+                Vivamus mi ex, placerat sed blandit vitae, ornare sed elit.
+                Curabitur sit amet venenatis sem.
+              </p>
+              <div className="tab-titles mb-10 mt-5 flex">
+                <p className="tab-links">Skills</p>
+                <p className="tab-links">Experience</p>
+                <p className="tab-links">Education</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
